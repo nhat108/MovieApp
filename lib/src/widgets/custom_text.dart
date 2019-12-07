4883a8e843d81bbac43b6851c7985b62d49a52b7
nhat108 <minhnhat10081999@gmail.com>
2019-12-07 13:04:00 +0700

    chaged a bit at custom_text

--- lib/src/widgets/custom_text.dart
+++ lib/src/widgets/custom_text.dart
@@ -6,6 +6,7 @@ class CustomText extends StatelessWidget {
   final Color textColor;
   final double fontSize;
   final FontWeight fontWeight;
+  final int maxLines;
   final TextOverflow overflow;
   const CustomText(
     this.text,
@@ -13,13 +14,14 @@ class CustomText extends StatelessWidget {
       this.fontFamily,
       this.textColor,
       this.fontSize,
-      this.fontWeight, this.overflow})
+      this.fontWeight, this.overflow, this.maxLines=1})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
+      maxLines: maxLines,
       overflow: overflow??TextOverflow.ellipsis,
       style: TextStyle(
           fontFamily: fontFamily,
