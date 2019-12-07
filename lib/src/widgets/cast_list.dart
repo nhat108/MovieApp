83ffbe615f4a830b8a584bd91673b8496fc2496f
nhat108 <minhnhat10081999@gmail.com>
2019-12-07 13:01:24 +0700

    fixed some small bugs at cast_list

--- lib/src/widgets/cast_list.dart
+++ lib/src/widgets/cast_list.dart
@@ -1,98 +1,85 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nux_movie/src/models/item_model.dart';
 import 'package:nux_movie/src/ui/person_page.dart';
 import 'package:nux_movie/src/utils/error.dart';
-
-// class CastList extends StatefulWidget {
-//   final List<Cast> casts;
-//   CastList({this.casts});
-//   @override
-//   State<StatefulWidget> createState() => CastListState();
-// }
+import 'package:nux_movie/src/widgets/custom_cached_network_image.dart';
 
 class CastList extends StatelessWidget {
-  final List<Cast>casts;
+  final List<Cast> casts;
 
   const CastList({Key key, this.casts}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
-      // padding: EdgeInsets.all(10.0),
       decoration: BoxDecoration(
           color: Color(0xFF2d3447), borderRadius: BorderRadius.circular(5.0)),
       child: Container(
         height: 180,
         child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: casts.length - 1,
-          itemBuilder: (context, index) {
-            return GestureDetector(
+            scrollDirection: Axis.horizontal,
+            itemCount: casts.length,
+            itemBuilder: (context, index) {
+              return _item(casts[index], context);
+            }),
+      ),
+    );
+  }
 
-              onTap: () {
-                String path=casts[index].profilePath;
-
-                openPersonPage(
-                    casts[index].id, path==null?'':path,context);
-              },
-              child: Container(
-                margin: EdgeInsets.all(8),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(5.0),
-                      child: CachedNetworkImage(
-                        imageUrl:
-                            'https://image.tmdb.org/t/p/w185/${casts[index].profilePath}',
-                        placeholder: (context, url) => Container(
-                          width: 78,
-                          height: 110,
-                          color: Colors.grey,
-                        ),
-                        errorWidget: (context, url, error) => ErrorUtils.getErrorPersonCard(),
-                        width: 78,
-                        height: 110,
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 4,
-                    ),
-                    Container(
-                      width: 78,
-                      child: Text(
-                        casts[index].name,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        textAlign: TextAlign.justify,
-                        style: TextStyle(
-                          fontSize: 12,
-                        ),
-                      ),
-                    ),
-                    Container(
-                        width: 78,
-                        child: Text(
-                          casts[index].character,
-                          maxLines: 2,
-                          style:
-                              TextStyle(fontSize: 10, color: Color(0xFFffe3d8)),
-                        ))
-                  ],
+  _item(Cast cast, BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _navigateToPersonPage(cast.id, cast.profilePath??'', context);
+      },
+      child: Container(
+        margin: EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(5.0),
+              child: CustomCachedNetWorkImage(
+                photoUrl: cast.profilePath,
+                width: 78,
+                height: 110,
+                errorWidget: Container(
+                  width: 78,
+                  height: 110,
+                  child: ErrorUtils.getErrorPersonCard(),
                 ),
               ),
-            );
-          },
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Container(
+              width: 78,
+              child: Text(
+                cast.name,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            Container(
+                width: 78,
+                child: Text(
+                  cast.character,
+                  maxLines: 2,
+                  style: TextStyle(fontSize: 10, color: Color(0xFFffe3d8)),
+                ))
+          ],
         ),
       ),
     );
   }
 
-  openPersonPage(int id, String profilePath,context) {
+  _navigateToPersonPage(int id, String profilePath, context) {
     Navigator.push(
       context,
       MaterialPageRoute(
