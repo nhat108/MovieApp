4f33ed293fe47153f4eaede7708c4b4fa64c3cc2
nhat108 <minhnhat10081999@gmail.com>
2019-12-11 21:31:04 +0700

    fixed

--- lib/src/widgets/custom_cached_network_image.dart
+++ lib/src/widgets/custom_cached_network_image.dart
@@ -2,19 +2,19 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 
 class CustomCachedNetWorkImage extends StatelessWidget {
-  final int pixels;
+  final String pixels;
   final double borderRadius;
   final double width;
   final double height;
   final String photoUrl;
   final Widget errorWidget;
-  const CustomCachedNetWorkImage({Key key, this.pixels=185, this.borderRadius=4, this.width, this.height,@required this.photoUrl, this.errorWidget}) : super(key: key);
+  const CustomCachedNetWorkImage({Key key, this.pixels='w185', this.borderRadius=4, this.width, this.height,@required this.photoUrl, this.errorWidget}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(borderRadius),
       child: CachedNetworkImage(
-        imageUrl: 'https://image.tmdb.org/t/p/w$pixels/$photoUrl',
+        imageUrl: 'https://image.tmdb.org/t/p/$pixels/$photoUrl',
         width: width,
         height: height,
         fit: BoxFit.cover,
