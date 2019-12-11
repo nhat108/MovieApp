7a7cad3b366130fc62fe5aa6c606886cbb94971d
nhat108 <minhnhat10081999@gmail.com>
2019-12-11 21:32:16 +0700

    commit

--- lib/src/widgets/list_movie.dart
+++ lib/src/widgets/list_movie.dart
@@ -1,14 +1,13 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:nux_movie/src/models/item_model.dart';
 import 'package:nux_movie/src/ui/movie_detail.dart';
-import 'package:nux_movie/src/utils/utils.dart';
+import 'package:nux_movie/src/widgets/item_movie_vertical.dart';
 
 class ListMovie extends StatefulWidget {
   final List<Result> results;
   final String kind;
-  ListMovie({this.results,this.kind});
+  ListMovie({this.results, this.kind});
   @override
   State<StatefulWidget> createState() => ListMovieState();
 }
@@ -18,23 +17,27 @@ class ListMovieState extends State<ListMovie> {
   Widget build(BuildContext context) {
     return Container(
       height: 280,
-       padding: EdgeInsets.symmetric(horizontal: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: widget.results.length,
         itemBuilder: (context, i) {
-          String tag='${widget.kind}+${widget.results[i].id}';
+          String tag = '${widget.kind}+${widget.results[i].id}';
+          Result result = widget.results[i];
           return Hero(
-            tag: tag,
-            child: _item(
-              result: widget.results[i],
-              tag: tag
-            ),
-          );
+              tag: tag,
+              child: ItemMovieVertical(
+                result: result,
+                tag: tag,
+                onTap: () {
+                  _navigateToMovieDetailPage(result: result, heroTag: tag);
+                },
+              ));
         },
       ),
     );
   }
+
   _navigateToMovieDetailPage({Result result, String heroTag}) {
     Navigator.push(
       context,
@@ -43,82 +46,6 @@ class ListMovieState extends State<ListMovie> {
                 result: result,
                 heroTag: heroTag,
               )),
-    );
-  }
-  Widget _item({Result result,String tag}) {
-    return GestureDetector(
-      onTap: () {
-        _navigateToMovieDetailPage(result: result,heroTag: tag);
-      },
-      child: Container(
-        key: Key('{$result.id}'),
-        width: 120,
-        margin: EdgeInsets.all(7),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              flex: 8,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(4.0),
-                child: CachedNetworkImage(
-                  imageUrl:
-                      'https://image.tmdb.org/t/p/w185/${result.posterPath}',
-                  width: 120,
-                  height: 170,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Expanded(
-              flex: 2,
-              child: Text(
-                '${result.title}',
-                maxLines: 2,
-                style: TextStyle(
-                    color: Color(0xFFff877c),
-                    fontWeight: FontWeight.w700,
-                    fontSize: 16),
-              ),
-            ),
-            SizedBox(height: 3,),
-            Expanded(
-              flex: 1,
-              child: Text(
-                Utils.getGenresList(result.genreIds),
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    color: Color(0xFFffe3d8),
-                    fontSize: 12),
-              ),
-            ),
-            SizedBox(
-              height: 3,
-            ),
-            Expanded(
-              flex: 1,
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.star,
-                    color: Colors.yellow,
-                    size: 20,
-                  ),
-                  SizedBox(
-                    width: 3,
-                  ),
-                  Text('${result.voteAverage}')
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
